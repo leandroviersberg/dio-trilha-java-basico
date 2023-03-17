8820e9b153bd14022ee4e185f112dc8e653b8487
Leandro Amorim Viersberg <leandroviersberg@gmail.com>
2023-03-17 16:07:42 -0300

    Trabalahndo com metodos

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # dio-trilha-java-basico
 Repositório para exercícios Java básico pela DIO
+
+
+
+/*Operadores
+
+Operador de atribuição representado pelo simbolo de =
+Operador de atribuição 
+Operadores Aritméticos:
+
++(adição)
+-(subtração)
+*(multiplicação)
+/ (divisão)
+% (módulo)
+++ (incremento)
+-- (decremento)
+Operadores de Atribuição:
+
+= (atribuição simples)
++= (atribuição de adição)
+-= (atribuição de subtração)
+*= (atribuição de multiplicação)
+/= (atribuição de divisão)
+%= (atribuição de módulo)
+<<= (atribuição de deslocamento à esquerda)
+
+||"= (atribuição de deslocamento à direita)"
+
+&= (atribuição de AND bit a bit)
+^= (atribuição de OR exclusivo bit a bit)
+|= (atribuição de OR bit a bit)
+
+
+Operadores Relacionais:
+
+== (igual a)
+!= (diferente de)
+>"(maior que)"
+>= (maior ou igual a)
+< (menor que)
+<= (menor ou igual a)
+
+Operadores Lógicos:
+
+&& (e lógico)
+|| (ou lógico)
+! (negação lógica)
+
+
+Operadores Bitwise:
+
+& (AND bit a bit)
+| (OR bit a bit)
+^ (OR exclusivo bit a bit)
+~ (NOT bit a bit)
+<< (deslocamento à esquerda)
+||(deslocamento à direita)
+||| (deslocamento à direita sem sinal)
+
+Operadores de Ternário:
+
+?: (operador ternário)
+Além desses operadores, 
+também existem os operadores de instância (.) e de cast ((tipo)),
+ que são usados para acessar membros de objetos e converter tipos de dados, respectivamente.
+
+ * 
+ * 
+ */
--- tipo-variaveis/src/Operadores.java
+++ tipo-variaveis/src/Operadores.java
@@ -1,76 +1,66 @@
-/*Operadores
-
-Operador de atribuição representado pelo simbolo de =
-Operador de atribuição 
-Operadores Aritméticos:
-
-+(adição)
--(subtração)
-*(multiplicação)
-/ (divisão)
-% (módulo)
-++ (incremento)
--- (decremento)
-Operadores de Atribuição:
-
-= (atribuição simples)
-+= (atribuição de adição)
--= (atribuição de subtração)
-*= (atribuição de multiplicação)
-/= (atribuição de divisão)
-%= (atribuição de módulo)
-<<= (atribuição de deslocamento à esquerda)
-
-||"= (atribuição de deslocamento à direita)"
-
-&= (atribuição de AND bit a bit)
-^= (atribuição de OR exclusivo bit a bit)
-|= (atribuição de OR bit a bit)
-Operadores Relacionais:
-
-== (igual a)
-!= (diferente de)
-
-|"(maior que)"
-
-< (menor que)
-= (maior ou igual a)
-<= (menor ou igual a)
-
-Operadores Lógicos:
-
-&& (e lógico)
-|| (ou lógico)
-! (negação lógica)
-
-
-Operadores Bitwise:
-
-& (AND bit a bit)
-| (OR bit a bit)
-^ (OR exclusivo bit a bit)
-~ (NOT bit a bit)
-<< (deslocamento à esquerda)
-||(deslocamento à direita)
-||| (deslocamento à direita sem sinal)
-
-Operadores de Ternário:
-
-?: (operador ternário)
-Além desses operadores, 
-também existem os operadores de instância (.) e de cast ((tipo)),
- que são usados para acessar membros de objetos e converter tipos de dados, respectivamente.
-
- * 
- * 
- */
-
 public class Operadores {
     public static void main(String[] args) {
-        boolean variavel = false;
 
-        variavel = !variavel; // negação, ou seja, usando "!" tem a finalidade de inverter a variavel boolean
-        System.out.println(variavel);
+        /*
+         * Operadores Relacionais
+         * De forma mais precisa os Operadores relacionais definem se o 'Operando' à
+         * eaquerda é igual, diferente,
+         * menor, menor ou igual, maior ou igual ao da direita, sempre retornando o
+         * valor booleano no resultado.
+         * 
+         * ==Quando verificamos se as variaveis são IGUAIS
+         * != Quando veriificamos se as variaveis são DIFERENTES das outras
+         * > Quando veriificamos se a variavei é MAIOR QUE a outra
+         * >= Quando veriificamos se a variavel é MAIOR OU IGUAL a outra
+         * < Quando veriificamos se a variavel é MENOR QUE a outra
+         * <= Quando veriificamos se a variavel é MENOR OU IGUAL a outra
+         * 
+         * 
+         * 
+         */
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("numeroUm é igual ao numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente ao numeroDois? " + simNao);
+
+        // Operador Ternario
+
+        // int a, b; //As variveis
+        // a = 5;
+        // b = 6;
+
+        /*
+         * String resultado = ""; //Se (if) a for igual ao b, deve imprimir verdadeiro
+         * if(a==b)
+         * resultado = "verdadeiro";
+         * else //Senão(else) deve imprimir falso
+         * resultado = "falso";
+         */
+
+        /*
+         * De forma mais simplificada será a mesma coisa do modo acima, mas serão usadas
+         * a simbologia "?:"
+         * int a, b; //As variveis
+         * a = 5;
+         * b = 6;
+         * 
+         * String resultado = a==b ?"verdadeiro" : "falso";
+         * System.out.println(resultado);
+         */
+
+        // ___________________________________________________________________________________
+
+        // boolean variavel = false;
+
+        // variavel = !variavel; // negação, ou seja, usando "!" tem a finalidade de
+        // inverter a variavel boolean
+        // System.out.println(variavel);
 
         // int numero = 5;
         // numero = -numero;
