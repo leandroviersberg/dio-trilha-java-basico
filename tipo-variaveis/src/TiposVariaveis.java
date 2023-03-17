549f3051018b4104577257234732b10aa47f27ad
Leandro Amorim Viersberg <leandroviersberg@gmail.com>
2023-03-16 21:00:51 -0300

    Trabalhando com operadores

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // 8 tipos primitivos em java:
+        /*
+         * int, byte, short, long, float, double, boolean e char - estes
+         * tipos não são considerados objetos, e portanto representam valores brutos.
+         */
+
     }
 }
